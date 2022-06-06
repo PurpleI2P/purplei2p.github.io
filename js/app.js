e4337e6c780abae29af854049da6ab96344b92dc
R4SAS <r4sas@i2pmail.org>
2022-06-07 01:10:21 +0300

    always show download links block
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 function renderDownloadButton() {
-	var downloadCount = 0, dl_btn = document.getElementById("dl-btn"), all_dls = document.getElementById("all-dls");
+	var downloadCount = 0, dl_btn = document.getElementById("dl-btn");
 
 	if (window.navigator.platform.toUpperCase().startsWith("WIN")) {
 		dl_btn.innerText = document.webL10n.get("header-dl-btn-bundle", null, "Download I2PdBrowser");
 		dl_btn.setAttribute("href", "https://github.com/PurpleI2P/i2pdbrowser/releases/latest");
-		all_dls.style.display = "block";
 
 	} else if (window.navigator.userAgent.toUpperCase().indexOf("ANDROID") !== -1) {
 		dl_btn.innerText = document.webL10n.get("header.dl-btn-android", null, "Download for Android");
 		dl_btn.setAttribute("href", droid_asset[0].browser_download_url);
-		all_dls.style.display = "block";
 
 	} else {
 		dl_btn.innerText = document.webL10n.get("header-dl-btn", null, "Downloads");
