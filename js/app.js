0ce882515c6e369baf707049d89c8d0185548c88
R4SAS <r4sas@i2pmail.org>
2022-06-20 00:04:32 +0300

    fix download button for android
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function renderDownloadButton() {
 
 	} else if (window.navigator.userAgent.toUpperCase().indexOf("ANDROID") !== -1) {
 		dl_btn.innerText = document.webL10n.get("header.dl-btn-android", null, "Download for Android");
-		dl_btn.setAttribute("href", droid_asset[0].browser_download_url);
+		dl_btn.setAttribute("href", "https://github.com/PurpleI2P/i2pd-android/releases/latest");
 
 	} else {
 		dl_btn.innerText = document.webL10n.get("header-dl-btn", null, "Downloads");
